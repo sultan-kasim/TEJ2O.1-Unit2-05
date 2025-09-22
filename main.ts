b110bf919ccfa6473032dfcbed5803020b65be78
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-09-22 12:31:24 -0400

    get current temperature in K

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sultan
+ * Created on: Sep 2025
+ * This program gets the current temperature,in K.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//comment
